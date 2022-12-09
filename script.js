59d1e742274787d99637c3e092a721d5338e5ec0
Kush Surti <kushsurti05@gmail.com>
2022-12-09 12:41:26 +0530

    basic functionality added

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// your code here
+var submitButton = document.querySelector("[type='submit']");
+var nameInput = document.querySelector("#name");
+var yearInput = document.querySelector("#year");
+
+var result = document.querySelector(".result");
+submitButton.addEventListener("click", changeHeading);
+
+function changeHeading(event) {
+  event.preventDefault();
+  var nameValue = nameInput.value.trim();
+  var yearValue = yearInput.value.trim();
+
+  if (nameValue && yearValue) {
+    result.innerText = `?name=${nameValue}&year=${yearValue}`;
+  } else if (nameValue) {
+    result.innerText = `?name=${nameValue}`;
+  } else if (yearValue) {
+    result.innerText = `?year=${yearValue}`;
+  }
+}
